9f368660811e4e63b5adaee89c3db84e4fa965b9
Ahmed Salah Abdalhfaz <mazoka433@gmail.com>
2022-03-05 17:30:42 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+gotools["subfinder"]="go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
+gotools["nuclei"]="GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei"
+gotools["crobat"]="go get github.com/cgboal/sonarsearch/cmd/crobat"
+gotools["httpx"]="go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest"
+gotools["naabu"]="go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest"
+gotools["uncover"]="go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest"
+
+
